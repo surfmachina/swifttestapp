fed6be8b217885b9cd05a024adbf1b01ef5dfbb7
Thomas Carlson <surfmachina@gmail.com>
2019-04-20 21:42:47 +1000

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Thomas Carlson on 20/4/19.
+//  Copyright © 2019 SurfMachina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
